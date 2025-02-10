551db4933c9fd2153104e574611f4e7f0060553b
LiorB-D <liorbd@outlook.com>
2025-02-10 13:42:03 -0500

    Initial setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tap-ftp-files',
+    version='0.0.1',
+    description='hotglue tap for importing files from FTP',
+    author='hotglue',
+    url='https://hotglue.xyz',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_ftp_files'],
+    install_requires=[],
+    entry_points='''
+        [console_scripts]
+        tap-ftp-files=tap_ftp_files:main
+    ''',
+    packages=['tap_ftp_files']
+)
